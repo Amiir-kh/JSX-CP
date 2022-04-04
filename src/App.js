f2b6699ac24ccb3f06e438bbe8a2cbb9c9a460d4
Amir <Aikin@live.fr>
2022-04-04 10:06:53 +0200

    Jsx Cp

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import logo from './logo21.jpg'
+import './App.css';
+import ReactPlayer from 'react-player'
+function App() {
+  return (
+     <div className="App">
+     <div style={{maxWidth:"100vw"}}>
+
+<h1 className="title red">checkpoint-jsx</h1>
+<br/ >
+<img src="/logo22.jpg" alt=" logo22" />
+<br/ >
+<img src={logo} alt ="logo21"/>
+</div>
+<ReactPlayer url="https://www.youtube.com/watch?v=v2Tlrs9SsO8
+    </div>
+  );
+}
